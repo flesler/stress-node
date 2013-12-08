06839efdaebf7903ad3ce8b0807d275c106777ad
Ariel Flesler <aflesler@gmail.com>
2013-12-08 15:48:41 -0300

    1.0.0

--- README.md
+++ README.md
@@ -1,4 +1,59 @@
 stress-node
 ===========
 
+## Description
+
 Stress test library for Node JS
+
+## Installation
+
+Using npm:
+
+	$ npm install stress-node
+
+## Usage example
+
+```js
+var stress = require('stress-node');
+stress({
+	url: 'http://google.com',
+	method: 'GET',
+	amount: 1000,
+	concurrent: 5,
+	data: null,
+	progress: function(report) {
+	}
+}, function(report) {
+});
+```
+
+## The report object
+
+Both functions will receive a report object that looks like this:
+
+```js
+{ 
+	url: 'http://google.com',
+	ok: 990,
+	fail: 5,
+	completed: 995,
+	ongoing: 4,
+	pending: 1,
+	total: 1000,
+	start: [object Date],
+	end: [object Date],
+	responses: [
+		{ status:200, amount: 990 }
+	],
+	errors: [ 
+		{ code: 'ECONNREFUSED', amount: 4 },
+		{ code: 404, amount: 1 }
+	]
+}
+```
+
+# Stress server
+
+I also made a CLI server that runs a stress test using this library.
+
+Go check it out at https://github.com/flesler/stress-node-server.
---
+++ index.js
@@ -0,0 +1,99 @@
+var http = require('http'),
+	url = require('url');
+
+module.exports = function(opts, done) {
+	var u = url.parse(opts.url);
+
+	var info = {
+		hostname: u.hostname,
+		port: u.port,
+		path: u.path,
+		method: opts.method,
+		// TODO
+		headers: null
+	};
+
+	var responses = {}, errors = {};
+	var report = {
+		url: opts.url,
+		ok: 0,
+		fail: 0,
+		completed: 0,
+		ongoing: 0,
+		pending: opts.amount,
+		total: opts.amount,
+		start: new Date()
+	};
+
+	while (opts.concurrent--) {
+		request();
+	}
+
+	function request() {
+		if (report.pending <= 0) return;
+		report.pending--;
+		report.ongoing++;
+		
+		var req = http.request(info, function(res) {
+			var status = res.statusCode;
+			if (status >= 400) {
+				error(status);
+			} else {
+				success(status);
+			}
+		});
+
+		req.setTimeout(opts.timeout);
+		req.on('error', function(e) {
+			error(e.code);
+		});
+		req.end(opts.data);
+	}
+
+	function success(status) {
+		if (responses[status]) {
+			responses[status].amount++;
+		} else {
+			responses[status] = { status:status, amount:1 }
+		}
+
+		report.ok++;
+		completed();
+	}
+
+	function error(code) {
+		if (errors[code]) {
+			errors[code].amount++;
+		} else {
+			errors[code] = {code:code, amount:1};
+		}
+
+		report.fail++;
+		completed();
+	}
+
+	function completed() {
+		report.ongoing--;
+		report.completed++;
+		request();
+		
+		if (opts.progress) {
+			opts.progress(report);
+		}
+
+		if (report.completed < report.total) return;
+
+		report.end = new Date();
+		report.responses = values(responses);
+		report.errors = values(errors);
+		done(report);
+	}
+};
+
+function values(obj) {
+	var arr = [];
+	for (var key in obj) {
+		arr.push(obj[key]);
+	}
+	return arr;
+}
